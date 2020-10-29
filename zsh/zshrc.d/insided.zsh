9fb9ab011fe772fc11216711f30ee01764d8cea1
Alexander Timmermann <alex@timmermann.me>
2020-10-29 18:23:54 +0100

    feat(zsh): add some convenience functions for aws

--- zsh/zshrc.d/insided.zsh
+++ zsh/zshrc.d/insided.zsh
@@ -25,3 +25,89 @@ alias vdir="cd ${VAGRANT_DIR}"
 alias cbdir="cd ${PROJECT_ROOT}/community-backend"
 
 alias ccache="vssh clear_cache"
+
+if [[ -z "$AWS_PROFILE" ]]
+then
+  export AWS_PROFILE=staging
+fi
+
+aws_profile () {
+    prof=$(aws configure list-profiles | fzf --history=${HOME}/.cache/aws_profiles --reverse --height=10)
+
+    if [[ -z "$prof" ]]
+    then
+        unset AWS_PROFILE
+        return 1
+    fi
+
+    export AWS_PROFILE=$prof
+}
+
+alias ap="aws_profile"
+
+aws_region () {
+    prof=$(aws ec2 describe-regions | fzf --history=${HOME}/.cache/aws_profiles --reverse --height=10)
+
+    if [[ -z "$prof" ]]
+    then
+        unset AWS_PROFILE
+        return 1
+    fi
+
+    export AWS_PROFILE=$prof
+}
+
+alias ap="aws_profile"
+
+shorten_region() {
+  local long_region=${1:-AWS_REGION}
+
+  echo "$long_region" | gsed -rn 's/(\w{2})\-(\w{1})\w+\-([0-9]{1})/\1\2\3/p'
+}
+
+function ec2 () {
+  if [[ -z $AWS_PROFILE ]]
+  then
+    aws_profile
+  fi
+
+  local selected_instance
+  local private_ip
+  local public_ip
+  local ssh_host
+  local short_region
+
+  selected_instance=$(aws ec2 describe-instances \
+    | jq '.Reservations | [.[] | .Instances] | flatten | [.[] | {Tags: [.Tags[]? | select(.Key == "Environment" or .Key == "Name") | .Value ] | sort, PrivateIpAddress, PublicIpAddress, InstanceId, ImageId, InstanceType, State: .State.Name}] | .[]' -c \
+    | sort -u \
+    | fzf --sync --preview "echo {} | jq ." --preview-window 'up:40%')
+
+  if [[ -z "$selected_instance" ]]
+  then
+    return 1
+  fi
+
+  private_ip=$(echo $selected_instance | jq -r .PrivateIpAddress)
+  ip=$(echo $selected_instance | jq -r .PublicIpAddress)
+
+  if [[ "$ip" == "null" ]]; then
+    ip=$private_ip
+  fi
+
+  if [[ -z $AWS_REGION ]]
+  then
+    export AWS_REGION=$(aws configure get region)
+  fi
+
+  short_region=$(shorten_region $AWS_REGION)
+
+  if [[ -z $short_region ]]
+  then
+    echo "Cannot determine jump host for '$AWS_REGION'"
+    return 1
+  fi
+
+  echo ">> ssh ${AWS_PROFILE}-${short_region} > ssh ${ip}"
+
+  ssh -tA $AWS_PROFILE-$short_region -- ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ip
+}
